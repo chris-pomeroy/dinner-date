0e13b98d88cff3b53134d9db49e6820d1976f8b6
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-02-12 16:38:07 -0700

    Added swipe tab

--- frontend/app/(tabs)/swipe.tsx
+++ frontend/app/(tabs)/swipe.tsx
@@ -9,7 +9,6 @@ export default function HomeScreen() {
             <SafeAreaView style={{
                 alignItems: "center",
                 justifyContent: "center",
-                height: "100%"
             }}>
                 <Card></Card>
             </SafeAreaView>
--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,16 +1,51 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import {IconSymbol} from "@/components/ui/IconSymbol";
+import React from "react";
 
 export function Card() {
 
     return (
         <View style={{
-            borderStyle: "solid",
-            borderWidth: 1,
-            borderColor: "grey",
-            borderRadius: 10,
-            padding: 50
+            borderRadius: 20,
+            overflow: "hidden",
+            backgroundColor: "lightgrey",
+            width: "90%",
+            height: "90%"
         }}>
-            <Text>helloo</Text>
+            <View style={{
+                width: "100%",
+                height: "80%",
+                overflow: "hidden"
+            }}>
+                <Image
+                    style={{
+                        width: "100%",
+                        height: "100%"
+                    }}
+                    source={require('@/assets/images/salmon-poke.jpg')} />
+                <View style={{
+                    position: "absolute",
+                    bottom: 10,
+                    left: 10
+                }}>
+                    <Text style={{
+                        fontSize: 30,
+                        fontWeight: "bold",
+                        color: "white"
+                    }}>Salmon Poke Bowl</Text>
+                    <Text style={{color: "white"}}>A taste sensation you'll write home to your parents about</Text>
+                </View>
+            </View>
+            <View style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-around",
+                height: "20%"
+            }}>
+                <IconSymbol size={60} name="xmark.circle.fill" color="red" />
+                <IconSymbol size={60} name="heart.fill" color="green" />
+            </View>
+
         </View>
     );
 }
