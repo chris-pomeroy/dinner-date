1cad08378f9cb31561093afa36f26a68cabfe9a3
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-02-12 12:18:25 -0700

    Initial commit

---
+++ src/main/java/com/chris/dinnerdate/DinnerDateApplication.java
@@ -0,0 +1,13 @@
+package com.chris.dinnerdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnerDateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris/dinnerdate/DinnerDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.dinnerdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnerDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
