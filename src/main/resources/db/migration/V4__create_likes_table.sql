3d3916ef352c8a7311185160906b8b2f0cb70527
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-04-14 15:17:41 -0600

    Refactored schema

--- src/main/resources/db/migration/V4__create_likes_table.sql
+++ src/main/resources/db/migration/V4__create_likes_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE public.likes (
   id serial NOT NULL PRIMARY KEY,
   recipe_id INTEGER REFERENCES recipes(id),
-  time_stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  time_stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  user_id INTEGER REFERENCES users(id)
 );
