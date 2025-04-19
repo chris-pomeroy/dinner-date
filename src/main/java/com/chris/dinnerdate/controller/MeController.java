99d944046ea466d1b81e3034631eb1031e34d098
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-04-18 19:51:19 -0600

    Added me endpoint

---
+++ src/main/java/com/chris/dinnerdate/controller/MeController.java
@@ -0,0 +1,16 @@
+package com.chris.dinnerdate.controller;
+
+import com.chris.dinnerdate.config.UserContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeController {
+
+    public record MeResponse(String email) {}
+
+    @GetMapping("/me")
+    public MeResponse getMe() {
+        return new MeResponse(UserContext.getEmail());
+    }
+}
