984a034c0cd5ebae012813ce1d32707d93229bcc
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-04-21 16:09:25 -0600

    Don't retry on 401

--- frontend/hooks/queries/useMeQuery.ts
+++ frontend/hooks/queries/useMeQuery.ts
@@ -1,10 +1,16 @@
 import {useQuery} from '@tanstack/react-query';
-import {authFetch} from "@/api/authFetch";
+import {authFetch, FetchError} from "@/api/authFetch";
 
 export const useMeQuery = () => {
-    const query = useQuery<{ email: string }>({
+    const query = useQuery<{ email: string }, FetchError>({
         queryKey: ['me'],
-        queryFn: () => authFetch(`/me`)
+        queryFn: () => authFetch(`/me`),
+        retry: (failureCount, error) => {
+            if (error.status === 401) {
+                return false;
+            }
+            return failureCount < 3;
+        },
     });
 
     return {
