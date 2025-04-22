c69cf733243163eee7b13ef3b244d54b87dfe174
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-04-22 15:33:17 -0600

    Amended schema

--- src/main/resources/db/migration/V3__create_users_table.sql
+++ src/main/resources/db/migration/V3__create_users_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.users (
   id serial NOT NULL PRIMARY KEY,
-  first_name NOT NULL,
+  first_name text NOT NULL,
   email text NOT NULL UNIQUE,
   password text NOT NULL,
   session_id text UNIQUE
