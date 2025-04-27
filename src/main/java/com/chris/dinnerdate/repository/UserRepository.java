b09a314c44bfe752388b98dfd7469545a98100f1
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-04-26 18:00:06 -0600

    Removed annotations

--- src/main/java/com/chris/dinnerdate/repository/UserRepository.java
+++ src/main/java/com/chris/dinnerdate/repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -17,12 +16,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     @Modifying
-    @Transactional
     @Query("UPDATE User u SET u.sessionId = NULL WHERE u.id = :userId")
     void clearSession(Long userId);
 
     @Modifying
-    @Transactional
     @Query("UPDATE User u SET u.currentLobbyId = :currentLobbyId where u.id = :id")
     void updateCurrentLobbyIdById(Long currentLobbyId, Long id);
 }
