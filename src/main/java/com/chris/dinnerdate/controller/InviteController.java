cfbd2a1e3cb9d55534b3cd865b5f48cdf52dd11d
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-05-11 15:09:01 -0600

    Check isDev for invite token

--- src/main/java/com/chris/dinnerdate/controller/InviteController.java
+++ src/main/java/com/chris/dinnerdate/controller/InviteController.java
@@ -2,10 +2,7 @@ package com.chris.dinnerdate.controller;
 
 import com.chris.dinnerdate.service.InviteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.net.URLEncoder;
@@ -31,12 +28,15 @@ public class InviteController {
         inviteService.joinInvite(inviteToken);
     }
 
-    // TODO make this redirect to prod or dev
     @GetMapping("/join/{inviteToken}")
-    public RedirectView redirect(@PathVariable String inviteToken) {
+    public RedirectView redirect(@PathVariable String inviteToken, @RequestParam boolean isDev) {
         // Validate this is a Base64 string
         Base64.getUrlDecoder().decode(inviteToken);
         inviteToken = URLEncoder.encode(inviteToken, UTF_8);
-        return new RedirectView("exp://10.0.0.49:8081/--/my-account?token=" + inviteToken);
+
+        if (isDev) {
+            return new RedirectView("exp://10.0.0.49:8081/--/my-account?token=" + inviteToken);
+        }
+        return new RedirectView("exp://u.expo.dev/951e7181-6010-4cbf-8673-b5b2b3acf104?runtime-version=1.0.0&channel-name=main/--/my-account?token=" + inviteToken);
     }
 }
