535c9ffd424abe383d6f2af0077be61fccc11da1
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2025-05-12 22:26:50 -0600

    Hide splash screen after response from useMeQuery

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,7 +1,5 @@
 import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
-import {useFonts} from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import {useEffect} from 'react';
 import 'react-native-reanimated';
 
 import {useColorScheme} from '@/hooks/useColorScheme';
@@ -16,20 +14,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
     const colorScheme = useColorScheme();
-    const [loaded] = useFonts({
-        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    });
     const queryClient = new QueryClient()
-
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded]);
-
-    if (!loaded) {
-        return null;
-    }
 
     return (
         <QueryClientProvider client={queryClient}>
--- frontend/components/BaseLayout.tsx
+++ frontend/components/BaseLayout.tsx
@@ -4,17 +4,24 @@ import LoginScreen from "@/components/LoginScreen";
 import {useAuthContext} from "@/contexts/AuthContext";
 import {useMeQuery} from "@/hooks/queries/useMeQuery";
 import {useEffect} from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function BaseLayout() {
 
     const {isLoggedIn, setIsLoggedIn} = useAuthContext()
-    const {isSuccess} = useMeQuery()
+    const {isSuccess, isError} = useMeQuery()
 
     useEffect(() => {
         if (isSuccess) {
             setIsLoggedIn(true)
         }
     }, [isSuccess, setIsLoggedIn])
+
+    useEffect(() => {
+        if (isSuccess || isError) {
+            SplashScreen.hideAsync();
+        }
+    }, [isSuccess, isError]);
 
     if (!isLoggedIn) {
         return <LoginScreen />
